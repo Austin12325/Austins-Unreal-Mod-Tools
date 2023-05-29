1191fd203e75e5fb9d1426c83650328359c16301
Austin <Ajlimo1980@gmail.com>
2023-05-29 15:20:24 -0600

    Create open_engine.py

---
+++ source/operators/open_engine.py
@@ -0,0 +1,12 @@
+import bpy
+import os 
+import webbrowser
+
+engine_path = bpy.context.scene["sna_uet_engine_path"]
+filename = engine_path.split("\\")
+remove = len(filename[-1])
+open = engine_path[:-remove-1]
+
+webbrowser.open(os.path.realpath(open))
+
+
